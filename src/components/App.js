8faf9738c202afd4137386a3cf4e26a0858efe71
Ram <nagasitaram4@gmail.com>
2021-09-12 14:46:43 +0800

    subscribing to store

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,27 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-function App() {
-  return (
+class App extends React.Component {
+  componentDidMount(){
+    //make api call
+    //dispatch action
+
+    const {store} = this.props;
+    store.subscribe(() =>{
+      console.log('UPDATED');
+      this.forceUpdate();//not generally used will change in the future
+    });
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    })
+    console.log('STATE: ',store.getState())
+
+  }
+  render(){
+    const movies = this.props.store.getState();
+    console.log('RENDER')
+    return (
     <div className="App">
       <Navbar/>
       <div className="main">
@@ -13,12 +32,14 @@ function App() {
         </div>
       </div>
       <div className="list">
-        {data.map((movie,index) =>(
+        {movies.map((movie,index) =>(
           <MovieCard movie={movie} key={`movies-${index}`}/>
         ))}
       </div>
     </div>
-  );
+    );
+  
+  }
 }
 
 export default App;
